5a56459968d5d0a3f30884bff63a26a33bd98d9b
Misty De Meo <mistydemeo@gmail.com>
2019-02-03 15:18:10 +0100

    Add vendored copy of Molinillo

--- Library/Homebrew/load_path.rb
+++ Library/Homebrew/load_path.rb
@@ -6,4 +6,10 @@ unless $LOAD_PATH.include?(HOMEBREW_LIBRARY_PATH.to_s)
   $LOAD_PATH.push(HOMEBREW_LIBRARY_PATH.to_s)
 end
 
+molinillo_path = HOMEBREW_LIBRARY_PATH/"vendor/molinillo"
+
+unless $LOAD_PATH.include?(molinillo_path.to_s)
+  $LOAD_PATH.push(molinillo_path.to_s)
+end
+
 require "vendor/bundle-standalone/bundler/setup"
